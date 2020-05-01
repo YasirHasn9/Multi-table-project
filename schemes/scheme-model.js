6a907aebc7458bb224553a94509047eb189fd62d
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-01 13:27:14 -0700

    findById

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,7 +4,15 @@ function find() {
   return db("schemes"); // select * from schemes
 }
 
+function findById(id) {
+  // select * from schemes
+  // where id = ?
+  return db("schemes")
+    .where({ id })
+    .first();
+}
 
 module.exports = {
-  find
+  find,
+  findById
 };
