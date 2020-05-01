2bfeed0307939b7592720ceabd192c8e3e0ab728
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-01 12:47:41 -0700

    name and quantity

--- queries.sql
+++ queries.sql
@@ -11,5 +11,9 @@ ON o.ShipVia = s.id
 WHERE o."OrderDate" < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+SELECT p.ProductName , o."Quantity" FROM "OrderDetail" AS o
+JOIN Product as p
+ON o.ProductId = p.id
+WHERE o.OrderId = 10251
+ORDER BY p.ProductName;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
